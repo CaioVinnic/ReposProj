f979e4da1d730b8cc224af97ce4039efa7d2dec1
Caio Vinícius <caio_vinnic@outlook.com>
2022-04-08 19:31:16 -0300

    implementação da ViewModel

--- ProjetosC#/LanchesMac/LanchesMac/Repositories/Interfaces/ILancheRepository.cs
+++ ProjetosC#/LanchesMac/LanchesMac/Repositories/Interfaces/ILancheRepository.cs
@@ -4,7 +4,6 @@ namespace LanchesMac.Repositories.Interfaces
 {
     public interface ILancheRepository
     {
-
         IEnumerable<Lanche> Lanches { get; }
         IEnumerable<Lanche> LanchesPreferidos { get; }
         Lanche GetLancheById(int lancheId);
