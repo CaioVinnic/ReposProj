3c2322f43eb03bd5e4c17f17dc2e5a67728da409
Caio Vinícius <caio_vinnic@outlook.com>
2022-05-05 19:04:46 -0300

    Checkout do pedido

--- ProjetosCSharp/LanchesMac/LanchesMac/Context/AppDbContext.cs
+++ ProjetosCSharp/LanchesMac/LanchesMac/Context/AppDbContext.cs
@@ -13,5 +13,8 @@ namespace LanchesMac.Context
         public DbSet<Lanche> Lanches { get; set; }
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
 
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
+
     }
 }
---
+++ ProjetosCSharp/LanchesMac/LanchesMac/Repositories/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,11 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public interface IPedidoRepository
+    {
+
+        void CriarPedido(Pedido pedido);
+
+    }
+}
