66ff7f75057e1b1a3dea08a42eb638e16182eb48
Caio Vinícius <caio_vinnic@outlook.com>
2022-05-10 22:09:41 -0300

    Paginação e Relatório

--- ProjetosCSharp/LanchesMac/LanchesMac/Models/Lanche.cs
+++ ProjetosCSharp/LanchesMac/LanchesMac/Models/Lanche.cs
@@ -46,6 +46,7 @@ namespace LanchesMac.Models
         [Display(Name = "Estoque?")]
         public bool EmEstoque { get; set; }
 
+        [Display(Name = "Categoria")]
         public int CategoriaId { get; set; }
         public virtual Categoria Categoria { get; set; }
     }
--- ProjetosCSharp/LanchesMac/LanchesMac/Startup.cs
+++ ProjetosCSharp/LanchesMac/LanchesMac/Startup.cs
@@ -1,10 +1,12 @@
-﻿using LanchesMac.Context;
+﻿using LanchesMac.Areas.Admin.Servicos;
+using LanchesMac.Context;
 using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using LanchesMac.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
 
 namespace LanchesMac;
 public class Startup
@@ -42,6 +44,7 @@ public class Startup
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddTransient<IPedidoRepository, PedidoRepository>();
         services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+        services.AddScoped<RelatorioVendasServices>();
 
         services.AddAuthorization(options =>
         {
@@ -54,6 +57,12 @@ public class Startup
 
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
+
+        services.AddPaging(options =>
+        {
+            options.ViewName = "Bootstrap4";
+            options.PageParameterName = "pageindex";
+        });
 
         services.AddControllersWithViews();
 
